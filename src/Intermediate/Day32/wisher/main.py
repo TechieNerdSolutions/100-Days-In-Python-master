c54726a70429aa8c73a60e92cc223f1d344bd46b
rachamv <rkutalian@gmai.com>
2023-01-26 08:35:25 +0100

    Initial Commit

--- src/Intermediate/Day32/wisher/main.py
+++ src/Intermediate/Day32/wisher/main.py
@@ -4,8 +4,7 @@ import random
 import smtplib
 
 MY_EMAIL = "royalphoenixtnd@gmail.com"
-MY_PASSWORD = "Rfp0HQb$+bNai8FT74ch0#cL&fkF#Q+WT4e"
-
+MY_PASSWORD = "rvezgmuxbggtblil"
 today = datetime.now()
 today_tuple = (today.month, today.day)
 
@@ -13,9 +12,9 @@ data = pandas.read_csv("birthdays.csv")
 birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
-    file_path = f"wisher/quotes{random.randint(1,3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
+    file_path = f"src/Intermediate/Day32/wisher/quotes{random.randint(1,3)}.txt"
+    with open(file_path) as quotes_file:
+        contents = quotes_file.read()
         contents = contents.replace("[NAME]", birthday_person["name"])
 
     with smtplib.SMTP("smtp.gmail.com") as connection:
